97ea87dd0871d3c9dbe95a735c95ee2476897c4a
Student <student@makerss-air-2.mynet>
2020-06-04 16:57:24 +0100

    started adding bookmarks, tests failing

--- app.rb
+++ app.rb
@@ -10,10 +10,21 @@ class BookmarkManager < Sinatra::Base
 
     get '/bookmarks' do
       # Print the ENV variable
-      p ENV
+      #p ENV
       @bookmarks = Bookmark.all
       erb :bookmark
     end
+
+    get '/bookmark/new' do
+      erb :add_bookmark
+    end
+
+    post '/addbookmark' do
+      p params
+      p "Form data submitted to the /bookmarks route!"
+    end 
+
+
 
     run! if app_file == $0
 
---
+++ spec/features/creating_bookmarks_spec.rb
@@ -0,0 +1,9 @@
+feature 'add bookmark page' do
+  scenario 'user visualises the newly added bookmark' do
+    visit('/bookmarks') #we are on /bookmarks route
+    click_button ('Add Bookmark') #we are on /bookmarks route
+    fill_in('url', with: 'http://testbookmark.com') # we are on /bookmarks/new
+    click_button('Submit')
+    expect(page).to have_content('http://testbookmark.com')
+  end
+end
